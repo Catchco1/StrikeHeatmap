1fe8c11997a88173754514f5274ced6585c2b452
Catchco1 <cag107@gmail.com>
2023-03-30 18:20:28 +0000

    try new techniques for stripping the content out of the web scrape

---
+++ LaborAction.py
@@ -0,0 +1,6 @@
+class LaborAction:
+    def __init__(self, dateFrom, dateTo, Employer, LaborOrg):
+        self.dateFrom = dateFrom
+        self.dateTo = dateTo
+        self.Employer = Employer
+        self.LaborOrg = LaborOrg
--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup as bs
+from LaborAction import LaborAction
 
 firefox_options = Options()
 firefox_options.add_argument("--headless")
 driver = webdriver.Chrome(service=Service('/usr/local/bin/geckodriver'),
                           options=firefox_options)
 driver.get('https://striketracker.ilr.cornell.edu/')
-print(driver.page_source.encode("utf-8"))
+page_source = driver.page_source.encode("utf-8")
+
+soup = bs(page_source, 'html.parser')
+
+laborActionDivs = soup.find_all("div", {"class": "tab-content"})
+
+print(laborActionDivs[0].split("  ").strip())
+
 driver.quit()
