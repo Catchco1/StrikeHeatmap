bf7ea8744158cf6898c9c686b1c8b37da4558ce1
Connor Gephart <connor.gephart@jhuapl.edu>
2023-03-31 15:37:18 +0000

    successfully get employer, labor org, state, start date, and end date

--- LaborAction.py
+++ LaborAction.py
@@ -1,6 +1,9 @@
 class LaborAction:
-    def __init__(self, dateFrom, dateTo, Employer, LaborOrg):
+    def __init__(self, dateFrom, dateTo, Employer, LaborOrg, State):
         self.dateFrom = dateFrom
         self.dateTo = dateTo
         self.Employer = Employer
         self.LaborOrg = LaborOrg
+        self.State = State
+    def __str__(self):
+        return "Employer: " + self.Employer + "\nLabor Org: " + self.LaborOrg + "\nState: " + self.State + "\nStart Date: " + self.dateFrom + "\nEnd Date: " + self.dateTo
--- main.py
+++ main.py
@@ -15,6 +15,23 @@ soup = bs(page_source, 'html.parser')
 
 laborActionDivs = soup.find_all("div", {"class": "tab-content"})
 
-print(laborActionDivs[0].split("  ").strip())
+laborActions = []
+for div in range(0, 10):
+    divSplit = laborActionDivs[div].get_text(separator="|", strip=True).split('|')
+    index = [x for x, e in enumerate(divSplit) if "Employer" in e]
+    employer = divSplit[index[0]+1][2:]
+    index = [x for x, e in enumerate(divSplit) if "Labor Organization" in e]
+    laborOrg = divSplit[index[0]+1][2:]
+    index = [x for x, e in enumerate(divSplit) if "Start Date" in e]
+    dateFrom = divSplit[index[0]+1][2:]
+    index = [x for x, e in enumerate(divSplit) if "End Date" in e]
+    dateTo = divSplit[index[0]+1][2:]
+    index = [x for x, e in enumerate(divSplit) if "State" in e]
+    state = divSplit[index[0]+1][2:]
+    newAction = LaborAction(dateFrom, dateTo, employer, laborOrg, state)
+    laborActions.append(newAction)
+
+for i in range(0, 10):
+    print(laborActions[i])
 
 driver.quit()
