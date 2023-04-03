a276c5f20e2f767938fe71edfebe57ea20980f88
Catchco1 <cag107@gmail.com>
2023-04-03 20:08:14 +0000

    practice maps

---
+++ mapping.py
@@ -0,0 +1,12 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import pygris
+from pygris.utils import shift_geometry
+
+
+us = pygris.states(cb = True, resolution = "20m")
+us_rescaled = shift_geometry(us)
+us_rescaled.plot(figsize = (12, 8))
+
+plt.savefig("test.png")
