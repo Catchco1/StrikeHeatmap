5e0c268ee8f1a3dd07c926ccb443a2028c106301
Catchco1 <cag107@gmail.com>
2023-04-03 20:08:08 +0000

    very close to a working solution

--- main.py
+++ main.py
@@ -6,6 +6,62 @@ from LaborAction import LaborAction
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
+import pygris
+from pygris.utils import shift_geometry
+
+state_tracker = {
+    "ALABAMA": 0,
+    "ALASKA": 0,
+    "ARIZONA": 0,
+    "ARKANSAS": 0,
+    "CALIFORNIA": 0,
+    "COLORADO": 0,
+    "CONNECTICUT": 0,
+    "DELAWARE": 0,
+    "FLORIDA": 0,
+    "GEORGIA": 0,
+    "HAWAII": 0,
+    "IDAHO": 0,
+    "ILLINOIS": 0,
+    "INDIANA": 0,
+    "IOWA": 0,
+    "KANSAS": 0,
+    "KENTUCKY": 0,
+    "LOUISIANA": 0,
+    "MAINE": 0,
+    "MARYLAND": 0,
+    "MASSACHUSETTS": 0,
+    "MICHIGAN": 0,
+    "MINNESOTA": 0,
+    "MISSISSIPPI": 0,
+    "MISSOURI": 0,
+    "MONTANA": 0,
+    "NEBRASKA": 0,
+    "NEVADA": 0,
+    "NEW HAMPSHIRE": 0,
+    "NEW JERSEY": 0,
+    "NEW MEXICO": 0,
+    "NEW YORK": 0,
+    "NORTH CAROLINA": 0,
+    "NORTH DAKOTA": 0,
+    "OHIO": 0,
+    "OKLAHOMA": 0,
+    "OREGON": 0,
+    "PENNSYLVANIA": 0,
+    "RHODE ISLAND": 0,
+    "SOUTH CAROLINA": 0,
+    "SOUTH DAKOTA": 0,
+    "TENNESSEE": 0,
+    "TEXAS": 0,
+    "UTAH": 0,
+    "VERMONT": 0,
+    "VIRGINIA": 0,
+    "WASHINGTON": 0,
+    "WEST VIRGINIA": 0,
+    "WISCONSIN": 0,
+    "WYOMING": 0,
+    "DISTRICT OF COLUMBIA": 0,
+}
 
 # Selenium setup
 firefox_options = Options()
@@ -55,8 +111,22 @@ for div in range(0, len(laborActionDivs)):
     except:
         print(laborActionDivs[div].get_text(separator="|", strip=True).split('|'))
 
-# Setup link between map and data
-us_map = gpd.read_file("Shapefiles/States_shapefile.shp")
+# Count strikes for one month
+for action in laborActions:
+    if action.State_Name in state_tracker:
+        state_tracker[action.State_Name] += 1
 
-labor_data = pd.DataFrame([vars(x) for x in laborActions])
+labor_data = pd.DataFrame(state_tracker.items(), columns=['State_Name', 'Strikes'])
+
+# Setup link between map and data
+us_map = pygris.states(cb = True, resolution = "20m")
+us_rescaled = shift_geometry(us_map)
+
 map_and_data = us_map.merge(labor_data, on="State_Name")
+
+# Drawing the map!
+fix, ax = plt.subplots(1, figsize=(12, 8))
+plt.xticks(rotation=90)
+
+map_and_data.plot(column="Strikes", cmap="Reds", linewidth=0.4, ax=ax, edgecolor=".4")
+plt.savefig("test.png")
