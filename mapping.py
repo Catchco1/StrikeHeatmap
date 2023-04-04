bc9b62e8f9182616b87e363a366d1a95acc17662
Catchco1 <cag107@gmail.com>
2023-04-04 17:50:54 +0000

    more map practice

--- mapping.py
+++ mapping.py
@@ -7,6 +7,7 @@ from pygris.utils import shift_geometry
 
 us = pygris.states(cb = True, resolution = "20m")
 us_rescaled = shift_geometry(us)
+print(us_rescaled)
 us_rescaled.plot(figsize = (12, 8))
 
 plt.savefig("test.png")
